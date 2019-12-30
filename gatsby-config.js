6302fe770424b864b973d82f8bea3ea80755b31f
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-12-30 23:48:24 +0530

    fix: use the custome uploadcare widget for file upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,12 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    "gatsby-plugin-netlify-cms",
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     {
       resolve: "gatsby-plugin-mdx",
       options: {
